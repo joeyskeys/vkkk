3ffe88f69a5f4aae087d8db1b12c1fc973afb01a
joeyskeys <joey.skeys@gmail.com>
2022-04-19 20:23:24 +0800

    add assimp cmake module

--- src/asset_mgr/mesh_mgr.h
+++ src/asset_mgr/mesh_mgr.h
@@ -13,6 +13,7 @@ namespace vkkk
 class MeshMgr : public Singleton<MeshMgr> {
 public:
     void load_file(const fs::path &path, uint32_t flag);
+    void add_box(const void *min, const void *max);
 
 private:
     void process_node(aiNode *node, const aiScene *scene, uint32_t flag);

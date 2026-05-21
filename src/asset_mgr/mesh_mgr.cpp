ae006795de968911ee4ed9ba724b262cf09844d4
joeyskeys <joey.skeys@gmail.com>
2026-05-21 12:10:33 +0800

    remove deprecated things

--- src/asset_mgr/mesh_mgr.cpp
+++ src/asset_mgr/mesh_mgr.cpp
@@ -8,49 +8,6 @@
 namespace vkkk
 {
 
-MeshMgrDeprecated::MeshMgrDeprecated(VkWrappedInstance* i)
-    : ins(i)
-{}
-
-void MeshMgrDeprecated::process_node(aiNode *node, const aiScene *scene,
-    const std::vector<VERT_COMP>& cs)
-{
-    for (int i = 0; i < node->mNumMeshes; ++i) {
-        aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
-        MeshDeprecated m{ins, cs};
-        m.load(mesh);
-        meshes.emplace_back(std::move(m));
-    }
-
-    for (int i = 0; i < node->mNumChildren; ++i)
-        process_node(node->mChildren[i], scene, cs);
-}
-
-MeshDeprecated* MeshMgrDeprecated::load_file(const fs::path& path, const std::vector<VERT_COMP>& cs) {
-    if (!fs::exists(fs::absolute(path))) {
-        std::cerr << "file : " << path << "does not exist" << std::endl;
-        throw std::runtime_error("model file does not exist");
-    }
-
-    Assimp::Importer importer;
-    const aiScene *scene = importer.ReadFile(path.string().c_str(), aiProcess_Triangulate | aiProcess_JoinIdenticalVertices | aiProcess_OptimizeMeshes | aiProcess_OptimizeGraph);
-
-    if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
-        return nullptr;
-
-    auto idx = meshes.size();
-    process_node(scene->mRootNode, scene, cs);
-    return &meshes[idx];
-}
-
-void MeshMgrDeprecated::load(const std::vector<VERT_COMP>& cs, const uint32_t v, const char* vbuf,
-    const uint32_t vs, const uint32_t i, const char* ibuf, const uint32_t is)
-{
-    MeshDeprecated m{ins, cs};
-    m.load(v, vbuf, vs, i, ibuf, is);
-    meshes.emplace_back(std::move(m));
-}
-
 void MeshMgr::process_node(const std::string& name, aiNode *node, const aiScene *scene,
     const std::vector<VERT_COMP>& cs)
 {
--- src/asset_mgr/mesh_mgr.h
+++ src/asset_mgr/mesh_mgr.h
@@ -12,48 +12,6 @@ namespace vkkk
 
 class VkWrappedInstance;
 
-class MeshMgrDeprecated : public Singleton<MeshMgrDeprecated> {
-private:
-    MeshMgrDeprecated(VkWrappedInstance* i);
-    MeshMgrDeprecated(const MeshMgrDeprecated&) = delete;
-    MeshMgrDeprecated& operator= (const MeshMgrDeprecated&) = delete;
-    friend class Singleton<MeshMgrDeprecated>;
-
-public:
-    MeshDeprecated* load_file(const fs::path &path, const std::vector<VERT_COMP>& cs);
-    void  load(const std::vector<VERT_COMP>&, const uint32_t, const char*,
-        const uint32_t, const uint32_t, const char*, const uint32_t);
-    void add_box(const void *min, const void *max);
-
-    inline void pour_into_gpu() {
-        for (auto& mesh : meshes)
-            mesh.load_gpu();
-    }
-
-    inline void free_gpu_resources() {
-        for (auto& mesh : meshes)
-            if (mesh.gpu_loaded)
-                mesh.unload_gpu();
-    }
-
-    inline void emit_draw_cmds(VkCommandBuffer cmd_buf, VkPipelineLayout ppl_layout,
-        const VkDescriptorSet* sets)
-    {
-        for (auto& mesh : meshes)
-            mesh.emit_draw_cmd(cmd_buf, ppl_layout, sets);
-    }
-
-private:
-    void process_node(aiNode *node, const aiScene *scene,
-        const std::vector<VERT_COMP>& cs);
-
-public:
-    std::vector<MeshDeprecated>   meshes;
-
-private:
-    VkWrappedInstance*  ins;
-};
-
 class MeshMgr : public Singleton<MeshMgr> {
 private:
     MeshMgr() {}
